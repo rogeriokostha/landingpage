21797aadba5ec895e7e5786daa85a75a6b53ea52
Rogerio <admkostha@gmail.com>
2026-02-07 17:19:57 -0300

    alterando whitenoise

--- backend/settings.py
+++ backend/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # ==============================================================================
 # ARQUIVOS ESTÁTICOS (CSS, JS, IMAGES) - WHITENOISE
 # ==============================================================================
-
+WHITENOISE_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "core/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
