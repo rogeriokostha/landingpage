fe2bbfc85cf5de02f01aa5595bf77199c7798ed6
Rogerio <admkostha@gmail.com>
2026-02-07 17:37:05 -0300

    ajustando a foto de capa

--- backend/urls.py
+++ backend/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include  # <--- Importe o include
+from django.urls import path, include , re_path # <--- Importe o include
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),  # <--- Adicione esta linha
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 
